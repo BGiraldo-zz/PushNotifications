14a7d1bbe4232540268f2eebca35377b9e892e2a
Brayan Giraldo <brayan.giraldo@yuxiglobal.com>
2017-08-09 11:11:47 -0500

    abc

---
+++ PushNotifications/Yuxi.Notifications.Service/Services/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yuxi.Notifications.Service.Services
+{
+    public interface INotificationService
+    {
+
+
+
+    }
+}
